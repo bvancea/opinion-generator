8d97c2ceb1690a5e9bb5bb80c203e5c98dd71e6b
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2013-04-15 21:31:00 +0300

    Initial commit.

---
+++ src/main/java/generator/opinion/CSVGenerator.java
@@ -0,0 +1,129 @@
+package generator.opinion;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bogdan
+ * Date: 4/11/13
+ * Time: 11:59 PM
+ *
+ *
+ * Generates opinions in csv format. Its easier to post them to solr this way.
+ */
+@Component
+public class CSVGenerator {
+
+    private String[] holders;
+    private String[] target;
+
+    private String[] sentimentWords;
+
+    private static final int ITERATIONS = 25000000;
+
+    private static final Logger logger = LoggerFactory.getLogger(CSVGenerator.class);
+
+    public void generateCSVFile(String filename) {
+
+        try {
+            FileWriter writer = new FileWriter(filename);
+
+            List<String> names = new ArrayList<String>();
+            List<String> targets = new ArrayList<String>();
+            List<String> sentiments = new ArrayList<String>();
+            List<Double> orientations = new ArrayList<Double>();
+
+            parseWords("/opt/solr/words/names.txt", names);
+            parseWords("/opt/solr/words/nouns.txt", targets);
+            parseSentimentWords("/opt/solr/words/sentiments.txt", sentiments,orientations);
+
+            Random random = new Random();
+
+            logger.debug("Started generating data.");
+
+            System.out.println("Starting to generate " + ITERATIONS + " tuples.");
+
+            writer.append("id,holder,target,sentimentWord,sentimentOrientation,docId\n");
+
+            int percents = ITERATIONS / 10;
+            for (int i = 0; i < ITERATIONS; i++) {
+                if (i % percents == 0) {
+                    System.out.println( "Reached" + i/percents + "0%");
+                }
+                int index;
+                writer.append(i + ",");
+
+                index = random.nextInt(names.size());
+                writer.append(names.get(index) + ",");
+
+                index = random.nextInt(targets.size());
+                writer.append(targets.get(index) + "," );
+
+                index = random.nextInt(sentiments.size());
+                writer.append(sentiments.get(index) + ",");
+
+                index = random.nextInt(orientations.size());
+                writer.append(orientations.get(index).toString() + ",");
+
+                writer.append("document" + index + "\n");
+            }
+            System.out.println("Done. Generated " + ITERATIONS + " tuples.");
+
+            writer.flush();
+
+            writer.close();
+
+        } catch (IOException e) {
+            logger.error("Error writing csv file to " + filename, e);
+        }
+    }
+
+    public void parseWords(String fileName, List<String> words ){
+        File file = new File(fileName);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                words.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public void parseSentimentWords(String fileName, List<String> sentiments, List<Double> sentimentOrientations) {
+        File file = new File(fileName);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split("\t");
+
+                String sentimentWord = tokens[0];
+                Double sentimentOrientation = Double.parseDouble(tokens[1])/5;
+
+                sentiments.add(sentimentWord);
+                sentimentOrientations.add(sentimentOrientation);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
---
+++ src/test/java/generator/opinion/CSVGeneratorTest.java
@@ -0,0 +1,38 @@
+package generator.opinion;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bogdan
+ * Date: 4/12/13
+ * Time: 1:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class CSVGeneratorTest {
+
+    private static final String OUTPUT_DIR = "/opt/solr/";
+    private static final String OUTPUT_FILE = "opinions.csv";
+
+    @Autowired
+    private CSVGenerator generator;
+
+    @Test
+    public void testCreateCSVDocument() {
+
+        generator.generateCSVFile(OUTPUT_DIR + OUTPUT_FILE);
+    }
+
+
+}
